7dea607d546c505fe81c3825f620f9d4747fdde2
Laura OBrien <54803384+Lor1138@users.noreply.github.com>
2021-09-22 19:53:41 -0400

    added all files to repo

--- pretty.js
+++ pretty.js
@@ -1,6 +1,6 @@
 document.getElementById('change').onclick = changeColor;
 
 function changeColor(){
-    document.body.style.color = 'purple';
+    document.body.style.color = 'white';
     return false;
 }
